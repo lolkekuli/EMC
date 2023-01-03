3dcc5a2a55e5cde97021cdfd4b0ae09df2607174
lolkekuli <umidqurbanli999@gmail.com>
2023-01-03 09:27:20 -0800

    EMC

---
+++ config.js
@@ -0,0 +1,17 @@
+/*!
+ * Quara Theme by OllieJW (https://olliejw.me)
+ * License - https://olliejw.me/tos
+ */
+
+var server_name = "Bobertovo";
+var server_ip = "142.132.154.110";
+var server_port = 28373;
+var ip_copied = "IP copied!";
+
+var firefly_count = 20;
+
+
+vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
+vote("Minecraft Servers Org", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraftservers.org/");
+vote("Minecraft MP", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraft-mp.com/");
+vote("Top Minecraft Servers", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://topminecraftservers.org/");
